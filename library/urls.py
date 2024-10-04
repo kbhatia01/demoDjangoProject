494a264775d696f0d0c4d2059d9b52517b8a8d1f
Dushyant Baweja <dbaweja@gmail.com>
2024-10-04 22:57:47 +0530

    Add create_author functionality

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
 urlpatters = [
-    path('createbook/' views.create_book, name='createbook')
+    path('createbook/', views.create_book, name='createbook')
+    path('createauthor/', views.create_author, name='createauthor')
 ]
--- library/views.py
+++ library/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.respnose import Response
+from rest_framework.response import Response
 from library.serializer import BookSerializer
+from library.serializer import AuthorSerializer
 # Create your views here.
 
 @api_view(['POST'])
@@ -12,3 +13,11 @@ def create_book(request):
     if book_serializer.is_valid():
         book_serializer.save()
         return Response(book_serializer.data, status = status.HTTP_201_CREATED)
+    
+@api_view(['POST'])
+def create_author(request):
+    author_serializer = AuthorSerializer(data = request.data)
+
+    if author_serializer.is_valid():
+        author_serializer.save()
+        return Response(author_serializer.data, status = status.HTTP_201_CREATED)
