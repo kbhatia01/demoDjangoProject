336c09504ece515d3c101f75a1f9654a854a050a
Shreyas Myana <shreyasmyana1997@gmail.com>
2024-10-05 10:06:11 +0530

    implement getByID for book and author

--- library/urls.py
+++ library/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from library import views
 
 urlpatterns = [
-    path('createbook/', views.create_book, name='createbook'),
+    path('createBook/', views.create_book, name='createbook'),
     path('createAuthor/', views.create_author, name='createauthor'),
+    path('book/<int:id>/', views.get_book_by_id, name='getBookByID'),
+    path('author/<int:id>/', views.get_author_by_id, name='getAuthorByID'),
 ]
