f15fa31b0716c0f3cfcdcfbf7cd9df5906773dc8
Dushyant Baweja <dbaweja@gmail.com>
2024-10-07 20:48:07 +0530

    add getauthor and getbook functionality

--- library/urls.py
+++ library/urls.py
@@ -4,4 +4,6 @@ from library import views
 urlpatterns = [
     path('createbook/', views.create_book, name='createbook'),
     path('createauthor/', views.create_author, name='createauthor'),
+    path('getauthor/<int:id>', views.get_author, name='getauthor'),
+    path('getbook/<int:id>', views.get_book, name='getbook')
 ]
--- library/views.py
+++ library/views.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from library.serializer import BookSerializer
 from library.serializer import AuthorSerializer
+from library.models import Author, Book
+
 # Create your views here.
 
 @api_view(['POST'])
@@ -21,3 +23,23 @@ def create_author(request):
     if author_serializer.is_valid():
         author_serializer.save()
         return Response(author_serializer.data, status = status.HTTP_201_CREATED)
+    
+@api_view(['GET'])
+def get_author(request, id):
+    try:
+        author = Author.objects.get(pk=id)
+    except Author.DoesNotExist:
+        return Response({'error': 'Author Not Found'}, status = status.HTTP_404_NOT_FOUND)
+    author_serialized = AuthorSerializer(author)
+    return Response(author_serialized.data, status = status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def get_book(request, id):
+    try:
+        book = Book.objects.get(pk=id)
+    except Book.DoesNotExist:
+        return Response({'error': 'Book Not Found'}, status = status.HTTP_404_NOT_FOUND)
+    book_serialized = BookSerializer(book)
+    return Response(book_serialized.data, status = status.HTTP_200_OK)
+
