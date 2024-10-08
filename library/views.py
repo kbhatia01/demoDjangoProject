f0061cc81513694b9020e27dd3c800e7ca644cd2
Abhishek <abhisheksunkuru@gmail.com>
2024-10-08 22:19:54 +0530

    Add get api for book and author

--- library/views.py
+++ library/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 
 from library.serializer import BookSerializer, AuthorSerializer
 
-
+from .models import Book, Author
 # Create your views here.
 
 @api_view(['POST'])
@@ -30,5 +30,21 @@ def create_author(request):
 
     return Response(author_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# TODO: GET BOOK BY ID and return json response..
-# TODO: GET AUTHOR BY ID and return json response..
+@api_view(['GET'])
+def get_book_by_id(request,pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return Response({"detail": "Not found."},status=status.HTTP_404_NOT_FOUND)
+
+    book_serializer = BookSerializer(book)
+    return Response(book_serializer.data, status=status.HTTP_200_OK)    
+
+@api_view(['GET'])
+def get_author_by_id(request, pk):
+    try:
+        author = Author.objects.get(pk=pk)
+    except Author.DoesNotExist:
+        return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+    author_serilizer = AuthorSerializer(author)
+    return Response(author_serilizer.data, status=status.HTTP_200_OK)    
