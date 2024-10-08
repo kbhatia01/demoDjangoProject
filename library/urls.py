dc77216cd5496567fa6b04e53a4c5b75c6169289
Seshathri <seshathri2008@gmail.com>
2024-10-08 21:19:15 +0530

    added author and book

--- library/urls.py
+++ library/urls.py
@@ -5,4 +5,6 @@ from library import views
 urlpatterns = [
     path('createbook/', views.create_book, name='createbook'),
     path('createAuthor/', views.create_author, name='createauthor'),
+    path('getBook/', views.get_book, name='getbook'),
+    path('getAuthor/', views.get_author, name='getauthor'),
 ]
