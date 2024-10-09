5cd525470cac36ec9bc2a5da4c69f780d727e753
kbhatia01 <karanbhtiacs@gmail.com>
2024-10-09 22:18:06 +0530

    lazy loading

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+from library.models import Book
+from django.db import connection
+books = Book.objects.all()
+
+for book in books:
+    print(book.authors.name)
+
+print(len(connection.queries))
+
