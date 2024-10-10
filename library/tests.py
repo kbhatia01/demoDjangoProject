9c32bce6c29eb729adbc2d712c6e4ac42c92b328
kbhatia01 <karanbhtiacs@gmail.com>
2024-10-10 21:06:22 +0530

    changes

--- library/tests.py
+++ library/tests.py
@@ -6,6 +6,7 @@ from django.test import TestCase
 from library.models import Book
 from django.db import connection
 books = Book.objects.all()
+# books = Book.objects.select_related('authors')
 
 for book in books:
     print(book.authors.name)
