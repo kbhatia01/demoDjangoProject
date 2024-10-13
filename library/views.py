27582ef32ca7f095c34279c5885564c7f978d800
vinodaluvala <vinod.vinnu542@gmail.com>
2024-10-13 18:48:59 +0530

    Add GET api_view for Book and Author

--- library/views.py
+++ library/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from library.serializer import BookSerializer, AuthorSerializer
+from library.models import Book, Author
 
 
 # Create your views here.
@@ -32,4 +33,17 @@ def create_author(request):
 
 
 # TODO: GET BOOK BY ID and return json response..
+@api_view(['GET'])
+def get_books(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
 # TODO: GET AUTHOR BY ID and return json response..
+@api_view(['GET'])
+def get_authors(request):
+    authors = Author.objects.all()
+    serializer = AuthorSerializer(authors, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
